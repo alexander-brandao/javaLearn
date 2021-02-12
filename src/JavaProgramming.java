709a71537b080367a89095e7b75936f1226f9a0a
Alexander Costa Brandao <alex12a7x@gmail.com>
2021-02-12 20:59:46 -0300

    scanner max and min number

--- src/JavaProgramming.java
+++ src/JavaProgramming.java
@@ -3,30 +3,46 @@ import java.util.Scanner;
 public class JavaProgramming {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
 
-        int counter = 0;
-        int sum = 0;
+        Scanner scanner = new Scanner(System.in); // cria um objeto para receber dados de entrada do cosole
 
-        while(true){
-            int order = counter + 1;
-            System.out.println("Enter number #" + order + ":");
 
+        // variaveis auxiliares para um valor minino e outra para um valor maximo
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+       // boolean first = true;
+
+        while (true) {
+
+            System.out.println("Enter number");
             boolean isAnInt = scanner.hasNextInt();
 
             if(isAnInt){
+
                 int number = scanner.nextInt();
-                counter++;
-                sum += number;
-                if(counter == 10){
-                    break;
+
+             /*   if(first){
+                    first = false;
+                    min = number;
+                    max = number;
+                } */
+
+                if(number > max){
+                    max = number;
                 }
-                scanner.nextLine();
+
+                if(number < min){
+                    min = number;
+                }
+
             }else{
-                System.out.println("invalid number");
+                break;
             }
+
+            scanner.nextLine(); // handle input
         }
-        System.out.println("sum = " + sum);
-        scanner.close();
+
+        System.out.println("min= " + min + " max= " + max);
+        scanner.close(); // fecha a entrada de dados no console
     }
 }
