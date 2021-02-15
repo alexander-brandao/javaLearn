1da66d643a82a1bf4c6e0af7ade3e944566e6934
Alexander Costa Brandao <alex12a7x@gmail.com>
2021-02-14 22:18:27 -0300

    java classes part two

--- src/JavaProgramming.java
+++ src/JavaProgramming.java
@@ -4,6 +4,7 @@ public class JavaProgramming {
     public static void main(String[] args){
         Car porsche = new Car();
         Car holden = new Car();
-
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
     }
 }
