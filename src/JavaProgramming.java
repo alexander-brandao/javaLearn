8ddfe43c8b6801609d41e866c56d3243952bcbf8
Alexander Costa Brandao <alex12a7x@gmail.com>
2021-02-18 20:47:48 -0300

    Inheritance part one

--- src/JavaProgramming.java
+++ src/JavaProgramming.java
@@ -3,13 +3,31 @@ public class JavaProgramming {
 
     public static void main(String[] args){
 
-        // Bank Account
+        Animal animal = new Animal("Animal",1,1,5,5);
 
-        BankAccount alexanderAccount = new BankAccount();
-        // 0, "Alexander","alex@gmail.com", "12345","123456789"
+        Dog dog = new Dog("Yorkie",8,20,2,
+                4,1,20,"long silky");
 
-        System.out.println(alexanderAccount.getEmail());
-        System.out.println(alexanderAccount.getBalance());
+        dog.eat();
 
     }
+
+    // Bank Account
+
+    //BankAccount alexanderAccount = new BankAccount();
+    // 0, "Alexander","alex@gmail.com", "12345","123456789"
+
+    //System.out.println(alexanderAccount.getEmail());
+    //System.out.println(alexanderAccount.getBalance());
+
+         /* VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+
+        VipCustomer person2 = new VipCustomer("Alex","alex@gmail.com");
+        System.out.println(person2.getName());
+
+        VipCustomer person3 = new VipCustomer("Aurora","aurora@email.com",
+                1000.0);
+        System.out.println(person3.getName());
+         */
 }
