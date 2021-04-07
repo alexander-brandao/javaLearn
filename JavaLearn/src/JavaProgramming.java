8a1ab7c3041e7f2d5d549dd5ccefd221c37b8057
Alexander Costa Brandao <alex12a7x@gmail.com>
2021-04-07 17:09:21 -0300

    composition part 2

--- JavaLearn/src/JavaProgramming.java
+++ JavaLearn/src/JavaProgramming.java
@@ -2,17 +2,31 @@ import java.lang.String;
 public class JavaProgramming {
 
     public static void main(String[] args){
+    Dimensions dimensions = new Dimensions(20,20,5);
 
+    Case theCase = new Case("220B", "Dell",
+            "240",dimensions);
+
+    Monitor theMoniter = new Monitor("27inch", "Acer",
+            27, new Resolution(2540, 1440));
+
+    Motherboard theMotherboard = new Motherboard("BJ-200", "Asus",
+            4,4,"v2.44");
+
+    PC thePC = new PC(theCase,theMoniter,theMotherboard);
+
+    thePC.getMonitor().drawPixelAt(1500,1200,"red");
+    thePC.getMotherboard().loadProgram("Windows 1.0");
+    }
+
+
+    /*
     Outlander outlander = new Outlander(36);
     outlander.steer(45);
     outlander.accelerate(30);
     outlander.accelerate(20);
     outlander.accelerate(-42);
-
-    }
-
-
-
+    */
 
 
    // Animal animal = new Animal("Animal",1,1,5,5);
