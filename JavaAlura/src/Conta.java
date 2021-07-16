c3d7f04ea2cd14e08ceffa72e0d023c25c47acf3
Alexander Brandao <alex12a7x@gmail.com>
2021-07-16 18:27:10 -0300

    banco

---
+++ JavaAlura/src/Conta.java
@@ -0,0 +1,31 @@
+public class Conta {
+
+    //fields
+    private int numero;
+    private double saldo;
+    private double limite;
+    private String nome;
+
+    // Constructor
+    public Conta(int numero, double saldo, double limite, String nome) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.limite = limite;
+        this.nome = nome;
+    }
+
+    // methods
+    public boolean saca(double valor){
+        if(this.saldo < valor){
+            return false;
+        }else{
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+    }
+
+    void deposita(double quantidade){
+        this.saldo += quantidade;
+    }
+
+}
