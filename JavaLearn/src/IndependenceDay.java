d759f30c0c3d83ff617451955e86b6ad1c1dba1b
Alexander Brandao <alex12a7x@gmail.com>
2021-09-29 16:15:56 -0300

    new methods

---
+++ JavaLearn/src/IndependenceDay.java
@@ -0,0 +1,12 @@
+public class IndependenceDay extends Movie{
+    //constructor
+    public IndependenceDay(String name){
+        super("Independence Day");
+    }
+
+    // methods
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
---
+++ JavaLearn/src/Jaws.java
@@ -0,0 +1,10 @@
+public class Jaws extends Movie{
+
+    //constructor
+    public Jaws(){
+        super("Jaws");
+    }
+    public String plot(){
+        return "A shark eats lots of people";
+    }
+}
