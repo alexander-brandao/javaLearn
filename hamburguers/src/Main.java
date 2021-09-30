db346ec27558b91fe7d8e88d2e19c58da269c463
Alexander Brandao <alex12a7x@gmail.com>
2021-09-30 17:35:06 -0300

    challenge

---
+++ hamburguers/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] argv){
+        Hamburger hamburger = new Hamburger("Basic","Sausage", 3.56,"White");
+        // double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("tomato", 0.27);
+        System.out.println(" total price is " + hamburger.itemizeHamburger());
+    }
+}
