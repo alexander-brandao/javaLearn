d6ec735536cd41132b82bde0f580152170be270c
Alexander Brandao <alex12a7x@gmail.com>
2021-10-05 18:05:06 -0300

    sort values with array

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -16,10 +16,44 @@ public class Main {
         return values;
     }
 
+    // method that print values
+    public static void printArray(int[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println("Element " + i + ", typed value was " + array[i]);
+        }
+    }
+
+    //method  that sort values
+    public static int[] sortIntegers(int[] array){
+        //create a array with the same capacity
+        // as the array that is been passed to this method
+        int[] sortedArray = new int[array.length];
+
+        // create a copy of the array that is been passed
+        for(int i=0; i<array.length;i++){
+            sortedArray[i] = array[i];
+        }
+
+        // sort the numbers in descending order
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag = false;
+            for(int i=0; i<sortedArray.length-1;i++){
+                if(sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
     public static void main(String[] args){
         int[] myIntegers = getIntegers(6);
-        for(int i=0; i<myIntegers.length; i++){
-            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
-        }
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
     }
 }
