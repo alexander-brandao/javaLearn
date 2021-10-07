fbd244276c5eb89f3a7baca93861d1b795b74c78
Alexander Brandao <alex12a7x@gmail.com>
2021-10-07 18:50:16 -0300

    begin of minimun element challenge

---
+++ MinimunElement/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    //method that reads user-entered values
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+    }
+}
