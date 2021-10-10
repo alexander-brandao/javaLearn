47a234eb54fe273070b1960a4c18e9b97417b483
Alexander Brandao <alex12a7x@gmail.com>
2021-10-10 15:46:54 -0300

    minimum element continues

--- MinimunElement/src/Main.java
+++ MinimunElement/src/Main.java
@@ -7,5 +7,26 @@ public class Main {
 
     public static void main(String[] args){
 
+        System.out.println("Enter count:");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] returnedArray = readIntegers(count);
+    }
+
+    //method that read integers
+    private static int[] readIntegers(int count){
+
+        //count of numbers in the array
+        int[] array = new int[count];
+
+        // loop that read the numbers entered by the user
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            array[i] = number;
+        }
+        return array;
     }
 }
