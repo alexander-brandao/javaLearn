a2e90683c65abd1a8bae2c91601eebad610a8785
Alexander Brandao <alex12a7x@gmail.com>
2021-10-12 21:47:50 -0300

    Array List part 1

---
+++ ArrayList/src/GroceryList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    // array object
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    //method that add items to the Grocery List
+    public void addGroceryList(String item){
+            groceryList.add(item);
+    }
+
+    //method that print the grocery list
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i = 0; i< groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    //method that modify grocery items
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item" + (position+1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+}
